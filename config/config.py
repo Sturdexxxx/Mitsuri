6861de3fe538a02cf1bc7373603048d87f5cd690
Sturdexxxx <134261890+Sturdexxxx@users.noreply.github.com>
2023-06-16 01:49:31 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -16,16 +16,16 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
 
     INFOPIC = int(getenv('INFOPIC', "true"))
-    EVENT_LOGS = getenv('EVENT_LOGS', "-100")
+    EVENT_LOGS = getenv('EVENT_LOGS', "-1001800732275")
     WEBHOOK = int(getenv('WEBHOOK', "False"))
     ARQ_API_URL = getenv("ARQ_API_URL", "https://thearq.tech/")
     ARQ_API_KEY = getenv("ARQ_API_KEY", "")
     URL = getenv('URL', "")  # Does not contain token
     PORT = int(getenv('PORT', 5000))
     CERT_PATH = getenv("CERT_PATH")
-    API_ID = getenv('API_ID', "")
-    API_HASH = getenv('API_HASH', "")
-    DB_URI = getenv('DATABASE_URL', "")
+    API_ID = getenv('API_ID', "4665778")
+    API_HASH = getenv('API_HASH', "10e3ed833b0d09699973420d45359409")
+    DB_URI = getenv('DATABASE_URL', "postgres://fqiiaofs:v-Fbgtr09XIsiwR6D5q3qz4mcwxy9Img@silly.db.elephantsql.com/fqiiaofs")
     DONATION_LINK = getenv('DONATION_LINK', "https://t.me/xelcius")
     LOAD = getenv("LOAD", "").split()
     NO_LOAD = getenv("NO_LOAD", "rss").split()
@@ -42,21 +42,21 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
     SUPPORT_CHAT = getenv('SUPPORT_CHAT', "https://t.me/Mitsuri_BotSupport")
     SPAMWATCH_SUPPORT_CHAT = getenv('SPAMWATCH_SUPPORT_CHAT', "")
     SPAMWATCH_API = getenv('SPAMWATCH_API', " ")
-    REPOSITORY = getenv("REPOSITORY", "https://github.com/RimuruDemonlord/MitsuriRobot")
+    REPOSITORY = getenv("REPOSITORY", "https://github.com")
     IBM_WATSON_CRED_URL = getenv("IBM_WATSON_CRED_URL", "")
     IBM_WATSON_CRED_PASSWORD = getenv("IBM_WATSON_CRED_PASSWORD", "")
     TEMP_DOWNLOAD_DIRECTORY = getenv("TEMP_DOWNLOAD_DIRECTORY", "./")
     HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
     TELEGRAPH_SHORT_NAME = getenv("TELEGRAPH_SHORT_NAME", "xelcius")
     HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "")
-    BOT_NAME = getenv("BOT_NAME", "Asuka") # Name Of your Bot.4
-    BOT_USERNAME = getenv("BOT_USERNAME", "MitsuriRobot") # Bot Username
+    BOT_NAME = getenv("BOT_NAME", "Mitsuri") # Name Of your Bot.4
+    BOT_USERNAME = getenv("BOT_USERNAME", "MitsuriGroupBot") # Bot Username
     OPENWEATHERMAP_ID = getenv("OPENWEATHERMAP_ID", "") # From:- https://openweathermap.org/api
-    LOG_GROUP_ID = getenv('LOG_GROUP_ID', "-100")
-    BOT_ID = getenv("BOT_ID", "")
-    ERROR_LOGS = getenv("ERROR_LOGS", "-100") # Error Logs (Channel Ya Group Choice Is Yours) (-100)
+    LOG_GROUP_ID = getenv('LOG_GROUP_ID', "-1001800732275")
+    BOT_ID = getenv("BOT_ID", "6251112181")
+    ERROR_LOGS = getenv("ERROR_LOGS", "-1001800732275") # Error Logs (Channel Ya Group Choice Is Yours) (-100)
     STRICT_GMUTE = int(getenv('STRICT_GMUTE', "True"))
-    MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+    MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ok:lol@cluster1.udhzs7r.mongodb.net/?retryWrites=true&w=majority")
     DEBUG = int(getenv('IS_DEBUG', "False"))
     REDIS_URL = getenv("REDIS_URL", "") # REDIS URL (From:- Heraku & Redis)
-    OWNER_NAME = getenv("OWNER_NAME", "")
+    OWNER_NAME = getenv("OWNER_NAME", "U N K N O W N")
