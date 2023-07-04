f1c8b4e30555978c94351e7feffb6a0d3e7f04a9
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-07-05 01:09:29 +0530

    Update __init__.py

--- MitsuriRobot/__init__.py
+++ MitsuriRobot/__init__.py
@@ -224,6 +224,8 @@ pgram = Client(session_name, api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 print("Scanning AIO http session")
 aiohttpsession = ClientSession() 
 
+update_queue = Queue()
+
 #install arq
 print("Connecting ARQ Client")
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
